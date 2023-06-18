80094da996bd5b6ed55b8bb414f65037c2778006
정영호 <zer0kola321@gmail.com>
2023-06-18 14:15:35 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,14 +1,12 @@
 function solution(numbers) {
-  var answer = numbers
-    .replace(/zero/gi, 0)
-    .replace(/one/gi, 1)
-    .replace(/two/gi, 2)
-    .replace(/three/gi, 3)
-    .replace(/four/gi, 4)
-    .replace(/five/gi, 5)
-    .replace(/six/gi, 6)
-    .replace(/seven/gi, 7)
-    .replace(/eight/gi, 8)
-    .replace(/nine/gi, 9);
-  return Number(answer);
+    const number = {
+        zero: 0, one: 1, two: 2, three: 3, four: 4,
+        five: 5, six: 6, seven: 7, eight: 8, nine: 9
+    };
+
+    const answer = numbers.replace(/zero|one|two|three|four|five|six|seven|eight|nine/g, (v) => {
+        return number[v];
+    });
+
+    return +answer;
 }
