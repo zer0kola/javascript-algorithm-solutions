bbe7b7cc72615cfdc65a8d2b4d1c21882a5dbdac
정영호 <zer0kola321@gmail.com>
2024-05-16 01:16:24 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.79 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -3,8 +3,5 @@ function solution(A,B){
     
     A.sort((a,b) => a-b);
     B.sort((a,b)=> b-a);
-    for (let i =0; i< A.length ; i++){
-        answer += A[i] * B[i];
-    }
-    return answer
+    return A.reduce((acc, _, index) => acc += A[index] * B[index], 0);
 }
