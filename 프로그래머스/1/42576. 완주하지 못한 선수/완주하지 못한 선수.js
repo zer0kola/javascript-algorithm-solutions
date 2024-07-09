89d6445950260379c8b515d0a68de1e391eb427e
정영호 <zer0kola321@gmail.com>
2024-07-09 17:08:00 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 27.88 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -2,14 +2,14 @@ function solution(participant, completion) {
     const map = new Map();
 
     for(let i = 0; i < participant.length; i++) {
-        const 참가자 = participant[i];
-        const 완주자 = completion[i] ?? '';
+        let a = participant[i]; // 참가자
+        let b = completion[i] ?? ''; // 완주자
 
-        map.set(참가자, (map.get(참가자) || 0) + 1); // 참가했으면 1 증가
-        map.set(완주자, (map.get(완주자) || 0) - 1); // 완주했으면 1 감소
+        map.set(a, (map.get(a) || 0) + 1); // 참가했으면 1 증가
+        map.set(b, (map.get(b) || 0) - 1); // 완주했으면 1 감소
     }
 
     for(let [key, value] of map) {
-        if(value > 0) return key;
+        if(value > 0) return key; // 참가만 하고 완주는 못한 선수 반환
     }
 }
