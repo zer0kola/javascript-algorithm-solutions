c695c0c03a574d9af2d06ac8df8a437c65b99ba7
zer0kola <zer0kola321@gmail.com>
2024-12-04 03:01:12 +0900

    디렉토리 변경

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,9 +1,8 @@
 function solution(arr)
 {
-    const answer = [];
+    var answer = [];
     for (let i =0; i< arr.length; i++){
-        if (answer.length === 0) answer.push(arr[i]);
-        else if (answer[answer.length-1] === arr[i]) continue;
+        if (answer.length > 0 && answer[answer.length-1] === arr[i]) continue;
         else answer.push(arr[i]);
     }
     return answer;
